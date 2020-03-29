62dcd01c9e8fa3f5df5fa98e0d7609399d753a3f
ryanbieber <ryanbieber@protonmail.com>
2020-03-29 12:07:33 -0500

    init

---
+++ app.R
@@ -0,0 +1,114 @@
+#
+# This is a Shiny web application. You can run the application by clicking
+# the 'Run App' button above.
+#
+# Find out more about building applications with Shiny here:
+#
+#    http://shiny.rstudio.com/
+#
+
+library(shiny)
+
+# Define UI for application that draws a histogram
+ui <- fluidPage(
+  
+  # Application title
+  titlePanel("How long will it take to recover from a pandemic?", windowTitle = "Epidemic Look"),
+  
+  sidebarLayout(
+    
+    # Sidebar with a slider input
+    sidebarPanel(
+      sliderInput("beta",
+                  "Social Distancing Factor (Infection Rate):",
+                  min = 0,
+                  max = 5,
+                  value = 2.16,
+                  step = .01
+                  ),
+      
+      sliderInput("gamma",
+                   "Rate of Recovery (1/days):",
+                   min = 0,
+                   max = 1,
+                   value = .5,
+                   step = .01
+                  ),
+      
+      numericInput("time",
+                   "Timeline:",
+                   min = 0,
+                   max = 1000,
+                   value = 100)
+      
+    ),
+    
+    
+    # Show a plot of the generated distribution
+    mainPanel(
+      plotOutput("epidemic")
+    )
+  )
+)
+
+# Define server logic required to draw a histogram
+server <- function(input, output) {
+  
+  output$epidemic <- renderPlot({
+    library(deSolve)
+    library(ggplot2)
+    t <- input$time
+    b <- input$beta
+    g <- input$gamma
+    
+    ##ode solution
+    init <- c(S=1-1e-6,I=1e-6,R=0)
+    parameters <- c(bet=b,gamm=g)
+    time <- seq(0,t,by=t/(2*length(1:t)))
+    eqn <- function(time,state,parameters){
+      with(as.list(c(state,parameters)),{
+        dS <- -bet*S*I
+        dI <- bet*S*I-gamm*I
+        dR <- gamm*I
+        return(list(c(dS,dI,dR)))})}
+    
+    out<-ode(y=init,times=time,eqn,parms=parameters)
+    out.df<-as.data.frame(out)
+    
+    mytheme4 <- theme_bw() +
+      theme(text=element_text(colour="black")) +
+      theme(panel.grid = element_line(colour = "white")) +
+      theme(panel.background = element_rect(fill = "#B2B2B2"))
+    theme_set(mytheme4)
+    
+    ## ggplot2 data
+    title <- bquote("SIR Model")
+    subtit <- bquote(list(beta==.(parameters[1]),~gamma==.(parameters[2])))
+    res<-ggplot(out.df,aes(x=time))+
+      ggtitle(bquote(atop(bold(.(title)),atop(bold(.(subtit))))))+
+      geom_line(aes(y=S,colour="Susceptible"))+
+      geom_line(aes(y=I,colour="Infected"))+
+      geom_line(aes(y=R,colour="Recovered"))+
+      ylab(label="Proportion")+
+      xlab(label="Time (days)")+
+      theme(legend.justification=c(1,0), legend.position=c(1,0.5))+
+      theme(legend.title=element_text(size=12,face="bold"),
+            legend.background = element_rect(fill='#FFFFFF',
+                                             size=0.5,linetype="solid"),
+            legend.text=element_text(size=10),
+            legend.key=element_rect(colour="#FFFFFF",
+                                    fill='#C2C2C2',
+                                    size=0.25,
+                                    linetype="solid"))+
+      scale_colour_manual("Compartments",
+                          breaks=c("Susceptible","Infected","Recovered"),
+                          values=c("blue","red","darkgreen"))
+    
+    print(res)
+  })
+  
+}
+
+# Run the application 
+shinyApp(ui = ui, server = server)
+
---
+++ testcode.R
@@ -0,0 +1,54 @@
+SIR.model <- function(t, b, g){
+  library(deSolve)
+  library(ggplot2)
+  
+  ##ode solution
+  init <- c(S=1-1e-6,I=1e-6,R=0)
+  parameters <- c(bet=b,gamm=g)
+  time <- seq(0,t,by=t/(2*length(1:t)))
+  eqn <- function(time,state,parameters){
+    with(as.list(c(state,parameters)),{
+      dS <- -bet*S*I
+      dI <- bet*S*I-gamm*I
+      dR <- gamm*I
+      return(list(c(dS,dI,dR)))})}
+  
+  out<-ode(y=init,times=time,eqn,parms=parameters)
+  out.df<-as.data.frame(out)
+
+  mytheme4 <- theme_bw() +
+    theme(text=element_text(colour="black")) +
+    theme(panel.grid = element_line(colour = "white")) +
+    theme(panel.background = element_rect(fill = "#B2B2B2"))
+  theme_set(mytheme4)
+  
+  ## ggplot2 data
+  title <- bquote("SIR Model")
+  subtit <- bquote(list(beta==.(parameters[1]),~gamma==.(parameters[2])))
+  res<-ggplot(out.df,aes(x=time))+
+    ggtitle(bquote(atop(bold(.(title)),atop(bold(.(subtit))))))+
+    geom_line(aes(y=S,colour="Susceptible"))+
+    geom_line(aes(y=I,colour="Infected"))+
+    geom_line(aes(y=R,colour="Recovered"))+
+    ylab(label="Proportion")+
+    xlab(label="Time (days)")+
+    theme(legend.justification=c(1,0), legend.position=c(1,0.5))+
+    theme(legend.title=element_text(size=12,face="bold"),
+          legend.background = element_rect(fill='#FFFFFF',
+                                           size=0.5,linetype="solid"),
+          legend.text=element_text(size=10),
+          legend.key=element_rect(colour="#FFFFFF",
+                                  fill='#C2C2C2',
+                                  size=0.25,
+                                  linetype="solid"))+
+    scale_colour_manual("Compartments",
+                        breaks=c("Susceptible","Infected","Recovered"),
+                        values=c("blue","red","darkgreen"))
+  
+  print(res)
+  ggsave(plot=res,
+         filename=paste0("SIRplot_","time",t,"beta",b,"gamma",g,".png"),
+         width=8,height=6,dpi=180)
+  }
+
+SIR.model(70,1.4,0.3)
